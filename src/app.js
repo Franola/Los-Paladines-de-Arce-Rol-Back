4e548e7a6ec2ae4c970c0e35ce999d56d3cfd376
Francisco Ciarallo <fran.jc02@gmail.com>
2025-08-15 22:53:58 -0300

    Se agrega CORS

--- src/app.js
+++ src/app.js
@@ -9,12 +9,14 @@ import personajesRouter from './routes/personajes.router.js';
 import cookieParser from "cookie-parser";
 import passport from 'passport';
 import { iniciarPassport } from "./config/passport.config.js";
+import cors from 'cors';
 
 const PORT=3000;
 
 dotenv.config();
 const app=express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
